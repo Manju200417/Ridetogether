14b6c0f4a2a3e5072bfafdcd20ea0f17786e9f82
Manju200417 <https://github.com/Manju200417>
2026-03-01 20:57:06 +0530

    added html files and some changes

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 
 def register(request):
     if request.method == 'POST':
+        name = request.POST.get('name')
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
