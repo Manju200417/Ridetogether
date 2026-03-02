d8b2f89faad9c0ce181c1b3e42e31af45e23a4ae
Manju200417 <https://github.com/Manju200417>
2026-03-02 23:13:18 +0530

    Ride Creation working properly

--- ride/models.py
+++ ride/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ride(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    phone_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
--- ride/views.py
+++ ride/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Ride
 
 def dashboard(request):
     return render(request,'dashboard.html')
+
+@login_required
+def create_ride(request):
+    if request.method == 'POST':
+        source = request.POST.get("source")
+        destination = request.POST.get("destination")
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+        phone = request.POST.get("phone_number")
+
+        Ride.objects.create(
+            user=request.user,
+            source=source,
+            destination=destination,
+            date=date,
+            time=time,
+            phone_number=phone)
+        
+        return redirect("create_ride")
+
+    return render(request,'rides/create_ride.html')
