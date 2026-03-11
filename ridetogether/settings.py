80fa0698782a0cdc5a58dfcc447ee7953b58ad32
Manju200417 <https://github.com/Manju200417>
2026-03-11 12:43:17 +0530

    Allowed all host

--- ridetogether/settings.py
+++ ridetogether/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--1&leh96#60ho&8e$pdfqad-pdv)&ndg3dxne0dq*wk^=_b)rx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
